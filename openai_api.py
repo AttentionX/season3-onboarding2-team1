bc9cabc68ebfb925e411b9e3b889b7c2b0a0a7a5
Yeeun Song <esther021125@gmail.com>
2024-01-24 20:59:19 +0900

    food list from img

--- openai_api.py
+++ openai_api.py
@@ -1,59 +1,52 @@
 import base64
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+# client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+client = OpenAI(api_key="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")
+
+
 
 system_message = "You are a helpful assistant"
 
-
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
-
-
+        return base64.b64encode(image_file.read()).decode('utf-8')
+    
 def encode_frame(frame):
-    return base64.b64encode(frame).decode("utf-8")
-
-
+    return base64.b64encode(frame).decode('utf-8')
+    
 def encode_images(image_paths, frames=False):
     if frames is True:
-        return [
-            f"data:image/jpeg;base64,{encode_frame(synthesized_frame)}"
-            for synthesized_frame in image_paths
-        ]
-    return [
-        f"data:image/jpeg;base64,{encode_image(image_path)}"
-        for image_path in image_paths
-    ]
-
+        return [f"data:image/jpeg;base64,{encode_frame(synthesized_frame)}" for synthesized_frame in image_paths]    
+    return [f"data:image/jpeg;base64,{encode_image(image_path)}" for image_path in image_paths]
 
 def test_gpt():
     return chatgpt("What is the meaning of life?")
 
-
 def chatgpt(query, model="gpt-4-1106-preview"):
     response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": query,
-            }
-        ],
-        model=model,
-    )
-
+            messages=[
+                {
+                    "role": "user",
+                    "content": query,
+                }
+            ],
+            model=model
+        )
+    
     response = response.choices[0].message.content
-    return response
-
+    return response 
 
 def test_vision_api():
-    client = OpenAI()
+    client = OpenAI(
+        api_key="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"
+    )
 
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
@@ -61,10 +54,10 @@ def test_vision_api():
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "What’s in this image?"},
+                    {"type": "text", "text": "Return a list of foods in the refrigerator as they would appear in a Python list data structure."},
                     {
                         "type": "image_url",
-                        "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                        "image_url": "https://healsview.com/wp-content/uploads/2023/10/open-fridge-or-1024x683.jpg",
                     },
                 ],
             }
@@ -76,9 +69,15 @@ def test_vision_api():
 
 
 def vision_api(images, prompt):
-    content = [{"type": "text", "text": prompt}]
+    content = [{
+        "type": "text",
+        "text": prompt
+    }]
     for image in images:
-        content.append({"type": "image_url", "image_url": image})
+        content.append({
+            "type": "image_url",
+            "image_url": image
+        })
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=[
@@ -94,5 +93,8 @@ def vision_api(images, prompt):
 
 if __name__ == "__main__":
     # response = test_vision_api()
-    response = test_gpt()
+    # response = test_gpt()
+    # print(response)
+
+    response = test_vision_api()
     print(response)
